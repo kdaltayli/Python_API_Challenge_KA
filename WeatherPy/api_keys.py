110d90260d146d605bbc55e549fca51e82ef3cb6
Kursad Altayli <kdaltayli@gmail.com>
2020-07-19 20:41:56 -0400

    deleted API and GmapsAPIKEY

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "d31988c4747643e6cbfa38e3be559102"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyC1hpxhm-VBGnt9s-5BRL060jeYRpD7B_Q"
+g_key = ""
